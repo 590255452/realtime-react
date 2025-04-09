40cfcc25cc5ffecc8e884c1ceb78f3ab09b8d018
WangLuqi <wangluqi@WangLuqideMacBook-Pro.local>
2025-04-09 16:24:40 +0800

    one

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 // TODO 创建axios实例，基本路径 跨域请求携带cookie。precess.env用于nodejs后端，import.meta.env用于vite前端框架
 export const axiosInstanace = axios.create({
-    // baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-    baseURL: "http://localhost:5001/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://realtime-express-production.up.railway.app/api",
     withCredentials: true
 });
